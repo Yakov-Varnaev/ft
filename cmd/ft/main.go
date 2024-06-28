4327b5c2a39d7adcb667269a425a5a69e612ceab
Yakov-Varnaev <yakov.varnaev@gmail.com>
2024-06-28 21:45:50 +0300

    burn with fire

--- cmd/ft/main.go
+++ cmd/ft/main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"log/slog"
-
-	db "github.com/Yakov-Varnaev/ft/internal"
-	"github.com/Yakov-Varnaev/ft/internal/server"
-)
+import "fmt"
 
 func main() {
-	db.Init()
-
-	router := server.SetupRouter()
-
-	if err := router.Run(); err != nil {
-		slog.Error("Failed to run server: %v", err)
-	}
+	fmt.Println("Burn the shit with fire!")
 }
