8705179791ec0f76a72d2fe13dfd5d7590d4cbf2
Yakov-Varnaev <yakov.varnaev@gmail.com>
2024-07-02 00:22:02 +0300

    some tests

--- tests/handler_test.go
+++ tests/handler_test.go
@@ -1,33 +1,30 @@
 package tests
 
 import (
-	// "github.com/Yakov-Varnaev/ft/internal/server"
-	// "github.com/gin-gonic/gin"
-	// "net/http"
-	// "net/http/httptest"
+	"github.com/Yakov-Varnaev/ft/internal/server"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestHelloWorldHandler(t *testing.T) {
-	// s := &server.Server{}
-	// r := gin.New()
-	// r.GET("/", s.HelloWorldHandler)
-	// // Create a test HTTP request
-	// req, err := http.NewRequest("GET", "/", nil)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-	// // Create a ResponseRecorder to record the response
-	// rr := httptest.NewRecorder()
-	// // Serve the HTTP request
-	// r.ServeHTTP(rr, req)
-	// // Check the status code
-	// if status := rr.Code; status != http.StatusOK {
-	// 	t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
-	// }
-	// // Check the response body
-	// expected := "{\"message\":\"Hello World\"}"
-	// if rr.Body.String() != expected {
-	// 	t.Errorf("Handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
-	// }
+	s := server.New()
+	// Create a test HTTP request
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Create a ResponseRecorder to record the response
+	rr := httptest.NewRecorder()
+	// Serve the HTTP request
+	s.ServeHTTP(rr, req)
+	// Check the status code
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+	// Check the response body
+	expected := "{\"message\":\"Hello World\"}"
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
 }
---
+++ tests/utils_test.go
@@ -0,0 +1,47 @@
+package tests
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Yakov-Varnaev/ft/pkg/repository/utils"
+)
+
+func TestFilters_Prepare(t *testing.T) {
+	tests := []struct {
+		name  string
+		f     utils.Filters
+		want  string
+		want1 []interface{}
+	}{
+		{
+			name:  "One filter",
+			f:     utils.Filters{"key": "value"},
+			want:  "key = $1",
+			want1: []interface{}{"value"},
+		},
+		{
+			name:  "Two filters",
+			f:     utils.Filters{"key1": "value1", "key2": 1},
+			want:  "key1 = $1 AND key2 = $2",
+			want1: []interface{}{"value1", 1},
+		},
+		{
+			name:  "Three filters",
+			f:     utils.Filters{"key1": "value1", "key2": 1, "key3": true},
+			want:  "key1 = $1 AND key2 = $2 AND key3 = $3",
+			want1: []interface{}{"value1", 1, true},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := tt.f.Prepare()
+			if got != tt.want {
+				t.Errorf("Filters.Prepare() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("Filters.Prepare() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
