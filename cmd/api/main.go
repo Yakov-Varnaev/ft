346aa5684212cdd25373742968c7acd37f29b991
Yakov-Varnaev <yakov.varnaev@gmail.com>
2024-08-29 15:00:22 +0300

    move groups from previous solution

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"github.com/Yakov-Varnaev/ft/internal/config"
-	"github.com/Yakov-Varnaev/ft/internal/database"
-	"github.com/Yakov-Varnaev/ft/internal/server"
-)
+import "fmt"
 
 func main() {
-	cfg, err := config.New()
-	if err != nil {
-		panic(err.Error())
-	}
-
-	db := database.New(cfg.DB)
-	defer db.Close()
-
-	app := server.NewServer(db)
-	if err = app.Run(); err != nil {
-		panic(err.Error())
-	}
+	fmt.Println("Hi there.")
 }
--- deeznuts.md
+++
@@ -1,5 +0,0 @@
-# handler
-
-# service
-
-# repository
