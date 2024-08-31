171dda83aea5f4cc3035cf97c7d8ebae6576c0d5
Yakov-Varnaev <yakov.varnaev@gmail.com>
2024-08-31 23:21:43 +0300

    category setup

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -10,6 +10,6 @@ type GroupRepository interface {
 	Create(info *model.GroupInfo) (*model.Group, error)
 	List(pg pg.Pagination) ([]*model.Group, int, error)
 	Exists(filters utils.Filters) (bool, error)
-	Update(id string, info *model.GroupInfo) (*model.Group, error)
+	// Update(id string, info *model.GroupInfo) (*model.Group, error)
 	Delete(id string) error
 }
